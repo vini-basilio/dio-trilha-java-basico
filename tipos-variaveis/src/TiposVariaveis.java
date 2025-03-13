c40e1f7da756c517f9ea91f3ad3969ad0f638632
Vini <vini.ba.dev@gmail.com>
2025-03-13 09:37:33 -0300

    Anotações de tipos e variáveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,15 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int salarioMinimo = 2_500;
+        System.out.println("Salário minímo: " + salarioMinimo);
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        // casting
+        short numeroCurto2 = (short) numeroNormal;
+
+        // constante
+        final float PI = 3.14F;
+
     }
 }
